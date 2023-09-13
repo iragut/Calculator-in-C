06431cca878bb21a82d9034edeab70497952bc87
iragut <74363823+iragut@users.noreply.github.com>
2023-09-13 21:03:56 +0300

    Add the possibility to calculate to infinity

--- main.c
+++ main.c
@@ -25,54 +25,73 @@ int Menu(){
 
 void NumericOperation(){
     double first_number, second_number, result;
-    char operand;
-    printf("Select the operation: +, -, *, /:\n");
-    scanf(" %c", &operand);
+    char operand, result_choice = 'y';
 
-    while (operand != '+' && operand != '-' && operand != '/' && operand != '*')
-    {
-        printf("Please select a valide operation!\n");
-        scanf(" %c", &operand);
-    }
-    printf("First Number: ");
+    printf("Input first number: ");
     scanf("%lf", &first_number);
-    printf("Second Number: ");
-    scanf("%lf", &second_number);
 
-    switch (operand)
+    while (result_choice == 'y')
     {
-    case '-':
-        result = first_number - second_number;
-        break;
-    case '+':
-        result = first_number + second_number;
-        break;
-    case '*':
-        result = first_number * second_number;
-        break;
-    case '/':
-        if (second_number == 0){
-            printf("Can't divide to 0!\n");
-            exit(0);
+        printf("Select the operation: +, -, *, /:\n");
+        scanf(" %c", &operand);
+
+        while (operand != '+' && operand != '-' && operand != '/' && operand != '*')
+        {
+            printf("Please select a valide operation!\n");
+            scanf(" %c", &operand);
         }
-        result = first_number / second_number;
-        break;
+
+        printf("Input second number: ");
+        scanf("%lf", &second_number);
+        while (operand == '/' &&  second_number == 0)
+        {
+            printf("Can't divide to 0!\n");
+            printf("Input second number: ");
+            scanf("%lf", &second_number);
+        }
+
+        switch (operand)
+        {
+        case '-':
+            result = first_number - second_number;
+            break;
+        case '+':
+            result = first_number + second_number;
+            break;
+        case '*':
+            result = first_number * second_number;
+            break;
+        case '/':
+            result = first_number / second_number;
+            break;
+        }
+        printf("%.2lf %c %.2lf = %.2lf\n", first_number, operand, second_number, result);
+        
+        printf("Continue and use the result as first number? (y/n)\n");
+        scanf(" %c", &result_choice);
+        first_number = result;
     }
-    printf("%.2lf %c %.2lf = %.2lf", first_number, operand, second_number, result);
 }
+
 void MatrixOperation(){
 
 }
 int main(){
-    int operation = Menu();
-    if (operation == 1)
+    int operation;
+    char continue_choice = 'y';
+    while (continue_choice == 'y')
     {
-        NumericOperation();
-    } 
-    else if (operation == 2)
-    {
-        MatrixOperation();
+        operation = Menu();
+        if (operation == 1)
+        {
+            NumericOperation();
+        } 
+        else if (operation == 2)
+        {
+            MatrixOperation();
+        }
+        printf("Want a new operation? (y/n)\n");
+        scanf(" %c", &continue_choice);
     }
-
     return 0;
 }
